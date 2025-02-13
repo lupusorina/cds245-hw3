7cad14a56e21f9ac70876d0a0f47c22a718f9318
Sorina Lupu <lupusorina@yahoo.com>
2025-02-12 21:21:28 -0800

    update test for sequence

--- tests/test_df_trajectory.py
+++ tests/test_df_trajectory.py
@@ -1,14 +1,14 @@
 import pandas as pd
-
+import numpy as np
 nb_episodes = 3
 duration = 10
 
 data = []
 for i in range(3):
     for j in range(duration):
-        data.append([i, j, [j + (i+1) * 100,
-                            j + (i+1) * 100,
-                            j + (i+1) * 100]])
+        action = np.array([j])
+        state = np.array([j + (i+1) * 100, j + (i+1) * 100, j + (i+1) * 100])
+        data.append([i, action, state.tolist()])
         
 # save data to csv
 col_names = ['episode', 'actions', 'states']
