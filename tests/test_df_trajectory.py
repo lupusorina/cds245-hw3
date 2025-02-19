1947e3f45148d69763fc790ddd772fec7e107bae
Sorina Lupu <lupusorina@yahoo.com>
2025-02-19 15:18:52 -0800

    cleanup tests

--- tests/test_df_trajectory.py
+++ tests/test_df_trajectory.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 
 DURATION = 10
+NB_EPISODES = 3
 
 data = []
-for i in range(3):
+for i in range(NB_EPISODES):
     for j in range(DURATION):
         action = np.array([j])
         state = np.array([j + (i+1) * 100, j + (i+1) * 100, j + (i+1) * 100])
